847c33fcb88a0d88193b5f567e9a2980cfe7b322
rogertgu <48294460+rogertgu@users.noreply.github.com>
2023-05-06 11:11:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-from flask import Flask
-from flask import render_template
-from flask import request
 import os
 
+from flask import Flask, flash, redirect, render_template, request
+
 app = Flask(__name__)
+app.config.update(
+    SECRET_KEY=os.environ.get("SECRET_KEY", "mysecretkey"),
+    # Otros ajustes de configuración si los necesitas
+)
 
 # rutas
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template("index.html")
 
-
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0',port=port,debug=True)
+    port = int(os.getenv("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
